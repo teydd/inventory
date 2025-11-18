51d4c1a010292d1369b20bd1900f9d330ae3c752
teydd <chemosted@gmail.com>
2025-11-18 23:23:02 +0300

    signin page

---
+++ app/sign-in/page.tsx
@@ -0,0 +1,16 @@
+import { SignIn } from "@stackframe/stack";
+import Link from "next/link";
+
+export default function SignInPage() {
+  return (
+    <>
+      <div className="min-h-screen flex items-center justify-center bg-gradient-to-r from-cyan-100 to-cyan-300">
+        <div className="max-w-md w-full space-y-8">
+          <SignIn />
+          <Link href="/">Return Home</Link>
+        </div>
+
+      </div>
+    </>
+  );
+}

7cd3156f104629c6d32bfa8547a0d37df81b4c80
teydd <chemosted@gmail.com>
2025-11-19 23:19:02 +0300

    product chart file

---
+++ components/products-chart.tsx
@@ -0,0 +1,12 @@
+import React from 'react'
+interface ChartData {
+    week:string
+    products:number
+}
+
+export default function ProductsChart({data}:{ data:ChartData[]}) {
+  return (
+    <>
+    </>
+  )
+}

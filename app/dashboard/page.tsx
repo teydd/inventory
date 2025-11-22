ab7564be279eaf8ffe54ed173a1a5a5540352b2f
teydd <chemosted@gmail.com>
2025-11-22 23:57:00 +0300

    updates

--- app/dashboard/page.tsx
+++ app/dashboard/page.tsx
@@ -148,7 +148,7 @@ export default async function DashboardPage() {
             <div className="flex items-center justify-between mb-6">
               <h2>New products per week</h2>
             </div>
-            <div className="h-48">
+            <div className="h-48 w-full">
               <ProductsChart data={weeklyProductsData} />
             </div>
           </div>

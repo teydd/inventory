c419642de8c1536b70d04bf3bac86d8313afcaf2
teydd <chemosted@gmail.com>
2025-11-23 23:44:29 +0300

    prettier

--- lib/auth.ts
+++ lib/auth.ts
@@ -1,13 +1,11 @@
 import { stackServerApp } from "@/stack/server";
 import { redirect } from "next/navigation";
 
-
-
 export async function getCurrentUser() {
-    const user = await stackServerApp.getUser()
-    if(!user){
-        redirect("/sign-in")
-    }
+  const user = await stackServerApp.getUser();
+  if (!user) {
+    redirect("/sign-in");
+  }
 
-    return user
+  return user;
 }

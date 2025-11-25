d6cc021697c4bb14e5c3a6e4d93954e4b7db12ff
teydd <chemosted@gmail.com>
2025-11-26 01:23:03 +0300

    update

--- app/settings/page.tsx
+++ app/settings/page.tsx
@@ -1,28 +1,35 @@
-import Sidebar from '@/components/sidebar'
-import { getCurrentUser } from '@/lib/auth'
-import React from 'react'
+import Sidebar from "@/components/sidebar";
+import { getCurrentUser } from "@/lib/auth";
+import { AccountSettings } from "@stackframe/stack";
+import React from "react";
 
 export default async function SettingsPage() {
-    const user = await getCurrentUser()
+  const user = await getCurrentUser();
   return (
     <>
-    <div className="min-h-screen bg-cyan-50">
-        <Sidebar currentPath='/settings'></Sidebar>
-        <main className="ml-64 p-6">
-        <div className="mb-8">
-          <div className="flex items-center justify-between">
-            <div>
-              <h1 className="text-2xl font-semibold text-gray-900">
-                Settings
-              </h1>
-              <p className="text-sm text-gray-500">
-Manage your account settings and preferences              </p>
+      <div className="min-h-screen bg-cyan-50">
+        <Sidebar currentPath="/settings"></Sidebar>
+        <main className="ml-64 p-8">
+          <div className="mb-8">
+            <div className="flex items-center justify-between">
+              <div>
+                <h1 className="text-2xl font-semibold text-gray-900">
+                  Settings
+                </h1>
+                <p className="text-sm text-gray-500">
+                  Manage your account settings and preferences
+                </p>
+              </div>
             </div>
           </div>
-        </div>     
-        </main>   
-    </div>    
+          <div className="max-w-6xl">
+            <div className="bg-white rounded-lg border border-cyan-200 p-6">
+                <AccountSettings fullPage></AccountSettings>
+
+            </div>
+        </div>
+        </main>
+      </div>
     </>
-   
-  )
+  );
 }
